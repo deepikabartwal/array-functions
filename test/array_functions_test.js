6c111b76303a5889e3bb81b50a7d8d712916e976
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-11-20 17:55:02 +0530

    migrated to mocha framework for tests
    edited my test file to make it compatible to mocha test framework.

--- test/array_functions_test.js
+++ test/array_functions_test.js
@@ -5,21 +5,12 @@ const identity = function(text){
   return text;
 }
 
-//testing map for identity function
-const testMapIdentityFunctions = function(){
-  assert.deepEqual(map(identity,[1]),[1]);
-  assert.deepEqual(map(identity,[]),[]);
-  assert.deepEqual(map(identity,[1,2]),[1,2]);
-  assert.deepEqual(map(identity,["a","b","c"]),["a","b","c"]);
-}
-
-const runTest = function(){
-  if(!map){
-    console.log("map function has not been implemented yet.Test won't run");
-    return;
-  }
-  testMapIdentityFunctions();
-  console.log("testMapIdentityFunctions passed");
-}
-runTest();
+describe('map',function(){
+  it('should return the array with same element as input',function(){
+    assert.deepEqual(map(identity,[1]),[1]);
+    assert.deepEqual(map(identity,[]),[]);
+    assert.deepEqual(map(identity,[1,2]),[1,2]);
+    assert.deepEqual(map(identity,["a","b","c"]),["a","b","c"]);
+  });
+});
 
