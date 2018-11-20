e041cf81d6eea3ce0934c36a81b2cb936727c8b8
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-11-20 19:41:47 +0530

    introduced test for mapping calculateLength func

--- test/array_functions_test.js
+++ test/array_functions_test.js
@@ -13,6 +13,10 @@ const square = function(number){
   return number*number;
 }
 
+const calculateLength = function(text){
+  return text.length;
+}
+
 describe('map',function(){
   it('should return the array with same element as input',function(){
     assert.deepEqual(map(identity,[1]),[1]);
@@ -31,6 +35,10 @@ describe('map',function(){
     assert.deepEqual(map(square,[]),[]);
     assert.deepEqual(map(square,[1,2,3]),[1,4,9]);
     assert.deepEqual(map(square,[2,4,6]),[4,16,36]);
+  });
+  it('should return length of each elements of array',function(){
+    assert.deepEqual(map(calculateLength,['a']),[1]);
+    assert.deepEqual(map(calculateLength,[]),[]);
   });
 });
 
