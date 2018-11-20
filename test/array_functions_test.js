d97e049b300136ff329c43d8d2634dc26e57ce6c
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-11-20 18:14:35 +0530

    introduced test for empty array for increment function mapping

--- test/array_functions_test.js
+++ test/array_functions_test.js
@@ -5,12 +5,20 @@ const identity = function(text){
   return text;
 }
 
+const increment = function(number){
+  return number+1;
+}
+
 describe('map',function(){
   it('should return the array with same element as input',function(){
     assert.deepEqual(map(identity,[1]),[1]);
     assert.deepEqual(map(identity,[]),[]);
     assert.deepEqual(map(identity,[1,2]),[1,2]);
     assert.deepEqual(map(identity,["a","b","c"]),["a","b","c"]);
+  });
+  it('should return increment values of elements in array',function(){
+    assert.deepEqual(map(increment,[1]),[2]);
+    assert.deepEqual(map(increment,[]),[]);
   });
 });
 
