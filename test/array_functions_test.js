2f943e81943f00a7fa4e66ecfcb48ab84d6be478
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-11-21 15:44:58 +0530

    edited the format of test writing

--- test/array_functions_test.js
+++ test/array_functions_test.js
@@ -43,40 +43,41 @@ const containsVowel = function(text){
     if(isVowel(text[letterPosition])){
       return true;
     }
-    return false;
   }
+  return false;
 }
 
 describe('map',function(){
-  it('should return the array with same element as input',function(){
-    assert.deepEqual(map(identity,[1]),[1]);
-    assert.deepEqual(map(identity,[]),[]);
-    assert.deepEqual(map(identity,[1,2]),[1,2]);
-    assert.deepEqual(map(identity,["a","b","c"]),["a","b","c"]);
+  describe('for single element array',function(){
+    it('should return single element array for single element array',function(){
+      assert.deepEqual(map(identity,[1]),[1]);
+      assert.deepEqual(map(increment,[1]),[2]);
+      assert.deepEqual(map(square,[2]),[4]);
+      assert.deepEqual(map(calculateLength,['a']),[1]);
+    });
   });
-  it('should return increment values of elements in array',function(){
-    assert.deepEqual(map(increment,[1]),[2]);
-    assert.deepEqual(map(increment,[]),[]);
-    assert.deepEqual(map(increment,[1,2,3]),[2,3,4]);
-    assert.deepEqual(map(increment,[23,55,67]),[24,56,68]);
+  describe('for empty array',function(){
+    it('should return empty array for empty array',function(){
+      assert.deepEqual(map(identity,[]),[]);
+      assert.deepEqual(map(increment,[]),[]);
+      assert.deepEqual(map(square,[]),[]);
+      assert.deepEqual(map(calculateLength,[]),[]);
+    });
   });
-  it('should return square of elements of array',function(){
-    assert.deepEqual(map(square,[2]),[4]);
-    assert.deepEqual(map(square,[]),[]);
-    assert.deepEqual(map(square,[1,2,3]),[1,4,9]);
-    assert.deepEqual(map(square,[2,4,6]),[4,16,36]);
-  });
-  it('should return length of each elements of array',function(){
-    assert.deepEqual(map(calculateLength,['a']),[1]);
-    assert.deepEqual(map(calculateLength,[]),[]);
-    assert.deepEqual(map(calculateLength,["ab"]),[2]);
-    assert.deepEqual(map(calculateLength,['a','it','is','a','cat']),[1,2,2,1,3]);
+  describe('for multiple element array',function(){
+    it('should return array with equal number of elements',function(){
+      assert.deepEqual(map(identity,[1,2]),[1,2]);
+      assert.deepEqual(map(identity,["a","b","c"]),["a","b","c"]);
+      assert.deepEqual(map(increment,[1,2,3]),[2,3,4]);
+      assert.deepEqual(map(square,[1,2,3]),[1,4,9]);
+      assert.deepEqual(map(calculateLength,['a','it','is','a','cat']),[1,2,2,1,3]);
+    });
   });
 });
 
 describe('filter',function(){
   it('should give only even numbers',function(){
-    assert.deepEqual(filter(isEven,[1]),[]);
+    assert.deepEqual(filter(isEven,[1]),[1]);
     assert.deepEqual(filter(isEven,[]),[]);
     assert.deepEqual(filter(isEven,[2]),[2]);
     assert.deepEqual(filter(isEven,[1,2]),[2]);
@@ -99,5 +100,8 @@ describe('filter',function(){
   it('should give text with vowels',function(){
     assert.deepEqual(filter(containsVowel,['bbq']),[]);
     assert.deepEqual(filter(containsVowel,[]),[]);
+    assert.deepEqual(filter(containsVowel,['are','bd','keep']),['are','keep']);
+    assert.deepEqual(filter(containsVowel,['athul','by','grr','bbq','durga','fry','cry','myth']),['athul','durga']);
   });
 });
+
